85802745fa7bd8509d82394d8b5858740455730d
govind <govindkumarjha2017@gmail.com>
2023-06-16 15:24:09 +0530

    l14 complete

--- L15/event delegation/script.js
+++ L15/event delegation/script.js
@@ -1,7 +1,13 @@
 const children= document.querySelectorAll('.child');
 
-for(let child of children){
-    child.addEventListener('click',(e)=>{
-        console.log(e.target.innerText);
-    })
-}
+// for(let child of children){
+//     child.addEventListener('click',(e)=>{
+//         console.log(e.target.innerText);
+//     })
+// }
+
+const parent=document.querySelector('.container');
+
+parent.addEventListener('click',(e)=>{
+    console.log(e.target.innerText);
+})
--- L15/event life cycle/script.js
+++ L15/event life cycle/script.js
@@ -1,7 +1,17 @@
-const grandparent=document.querySelector('grandparent');
+const grandparent=document.querySelector('.grandparent');
 const parent=document.querySelector('.parent');
 const child=document.querySelector('.child');
 
 grandparent.addEventListener('click',()=>{
-    console.log("grandparent clicked")
-})
+    console.log("grandparent div clicked!")
+},false)
+
+parent.addEventListener('click',(e)=>{
+    e.stopPropagation();
+    console.log("parent div clicked!")
+},false)
+
+child.addEventListener('click',(e)=>{
+    // e.stopPropagation();
+    console.log("child div clicked!");
+},false)
