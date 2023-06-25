5196b359c779847c476b8ecfc600e387b343bea6
govind <govindkumarjha2017@gmail.com>
2023-06-25 21:04:52 +0530

    L18-class

---
+++ L18/todo using jquery/script.js
@@ -0,0 +1,14 @@
+const inpvalue = $('#inp').val();
+$('button').on('click',(e)=>{
+    const str=`<div class="section-A">
+            <input type="checkbox">
+                <p>${inpvalue}</p>
+            </div>
+            <div class="section-B">
+                <span class="up-arrow">⬆️</span>
+                <span class="bin">🗑️</span>
+                <span class="down-arrow">⬇️</span>
+            </div>`
+
+    
+})
